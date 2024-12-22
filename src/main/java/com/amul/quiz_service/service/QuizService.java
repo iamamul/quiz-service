1075b2c4d9f5db3ade29e55dc33a80ccd73b2b27
Amul Katta <63243603+iamamul@users.noreply.github.com>
2024-12-22 12:47:08 +0530

    implement services to questionService

--- src/main/java/com/amul/quiz_service/service/QuizService.java
+++ src/main/java/com/amul/quiz_service/service/QuizService.java
@@ -38,29 +38,16 @@ public class QuizService {
     }
 
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
-//        Optional<Quiz> quiz = quizDao.findById(id);
-//        List<Question> questionsFromDB = quiz.get().getQuestions();
-        List<QuestionWrapper> questionsForUser = new ArrayList<>();
-//        for(Question q : questionsFromDB){
-//            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
-//            questionsForUser.add(qw);
-//        }
-//
-        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
 
+        Quiz quiz = quizDao.findById(id).get();
+        List<Integer> questionIds = quiz.getQuestionIds();
+        ResponseEntity<List<QuestionWrapper>> questions = questionServiceClient.getQuestionFromId(questionIds);
+        return questions;
     }
 
     public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
-//        Quiz quiz = quizDao.findById(id).get();
-//        List<Question> questions = quiz.getQuestions();
-        int right = 0;
-//        int i = 0;
-//        for(Response response : responses){
-//            if(response.getResponse().equals(questions.get(i).getRightAnswer()))
-//                right++;
-//
-//            i++;
-//        }
-        return new ResponseEntity<>(right, HttpStatus.OK);
+
+       ResponseEntity<Integer> score = questionServiceClient.getScore(responses);
+        return score;
     }
 }
